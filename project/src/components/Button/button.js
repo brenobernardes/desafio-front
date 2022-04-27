06aa2949f8829cfff7d5ca99ff1a9bacfe6eea21
Breno Bernardes <breno.pbernardes@gmail.com>
2022-04-26 22:01:05 -0300

    update estilos

--- project/src/components/Button/button.js
+++ project/src/components/Button/button.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-    background-color: #f4a950;
-    width: 110px;
-    height: 30px;
+    background-color: #ffff;
+    width: 150px;
+    height: 40px;
     border-radius: 5px;
     border: #f4a950;
     &:hover {
         transition: .3s;
-        background-color: #f7c282;
+        background-color: #000000;
+        color: #ffff;
     }
 `;
